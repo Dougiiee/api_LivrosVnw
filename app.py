b039e4f1966cd9f2498d24750cdf2a4c3650402b
Dougiiee <dodolaureano@gmail.com>
2025-03-20 12:01:21 -0300

    Primeiro commit da API que irá integrar o projeto VNW livros

---
+++ app.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, jsonify  # Aqui estamos importando a classe Flask do módulo flask para criar nossa aplicação
+
+# Criamos uma instância do Flask e armazenamos na variável "app"
+# O parâmetro __name__ indica que este arquivo será reconhecido como o principal da aplicação
+app = Flask(__name__)
+
+import sqlite3
+
+
+
+def initi_db():
+
+    # sqlite3 crie o arquivo database.db e se conect com a variavel conn (connection)
+    with sqlite3.connect('database.db') as conn:
+        conn.execute('''
+                CREATE TABLE IF NOT EXISTS LIVROS(
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    titulo TEXT NOT NULL,
+                    categoria TEXT NOT NULL,
+                    autor TEXT NOT NULL,
+                    image_url TEXT NOT NULL
+                    )
+    ''')
+
+initi_db()
+
+@app.route('/doar', methods = ['POST'])
+def doar():
+
+    dados = request.get_json()
+
+    print(f'AQUI ESTAO OS DADOS RETORNADOS DO CLIENTE {dados}')
+
+    titulo = dados.get('titulo')
+    categoria = dados.get('categoria')
+    autor = dados.get('autor')
+    image_url = dados.get('image_url')
+
+    if not titulo or not categoria or not autor or not image_url:
+        return jsonify({'erro':'Todos os campos sao obrigatorios'}), 400
+
+
+    with sqlite3.connect('database.db') as conn:
+        conn.execute(f'''
+        INSERT INTO LIVROS (titulo, categoria, autor, image_url)
+        VALUES ('{titulo}', '{categoria}', '{autor}', '{image_url}')
+        ''')
+
+    conn.commit()
+
+    return jsonify({'mensagem': 'Livro cadastrado com sucesso'}), 201
+# Aqui verificamos se o script está sendo executado diretamente e não importado como módulo
+if __name__ == "__main__":
+    # Inicia o servidor Flask no modo de depuração (nesse modo nossa API responde automaticamente a qualquer atualização que fizermos no código)
+    app.run(debug=True)
